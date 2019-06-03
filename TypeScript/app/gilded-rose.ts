6a157c8dab5038b841a8dce0ebe998bda02293fd
Chris Slater <chris@snapper.fish>
2019-06-03 22:38:30 +0100

    add Conjured scenario

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,21 +1,21 @@
-// const isImprovedWithTime = (item: Item) => (
-//     item.name === 'Aged Brie' || 
-//     item.name === 'Backstage passes to a TAFKAL80ETC concert'
-// )
+const condition = <T>(predicate: (p: T) => boolean, truthy: (p: T) => T, falsey: (p: T) => T) => (item: T): T => 
+    predicate(item) ? truthy(item) : falsey(item)
 
 const isExpired = (item: Item) => item.sellIn < 0
 
-const decreaseSellIn = (item: Item) => {
+const identity = <I>(i: I) => i 
+
+const decreaseSellIn = (item: Item): Item => {
     item.sellIn = item.sellIn - 1
     return item
 } 
 
-const reduceQualityToZero = (item: Item) => { 
+const reduceQualityToZero = (item: Item): Item => { 
     item.quality = 0
     return item
 }
 
-const increaseItemQuality = (item: Item) => {
+const increaseItemQuality = (item: Item): Item => {
     if (item.quality < 50) {
         item.quality = item.quality + 1
     }
@@ -23,7 +23,7 @@ const increaseItemQuality = (item: Item) => {
     return item
 }
 
-const decreaseItemQuality = (item: Item) => {
+const decreaseItemQuality = (item: Item): Item => {
     if (item.quality > 0) {
         item.quality = item.quality - 1
     }
@@ -61,35 +61,35 @@ export class GildedRose {
             switch(item.name) {
                 case 'Aged Brie':
                     item = increaseItemQuality(item)
-
-                    if (isExpired(item)) {
-                        item = increaseItemQuality(item)
-                    }
+                    item = condition(isExpired, increaseItemQuality, identity)(item)
+                    
                     return item
                 
                 case 'Backstage passes to a TAFKAL80ETC concert':
-                    item = increaseItemQuality(item)
-
-                    if (item.sellIn < 11) {
-                        item = increaseItemQuality(item)
-                    }
-
-                    if (item.sellIn < 6) {
-                        item = increaseItemQuality(item)
-                    }                    
-
                     if (isExpired(item)) {
                         item = reduceQualityToZero(item)
+                        return item
                     }
+
+                    item = increaseItemQuality(item)
+                    item = condition((item) => item.sellIn < 11, increaseItemQuality, identity)(item)
+                    item = condition((item) => item.sellIn < 6, increaseItemQuality, identity)(item)                                        
+
+                    return item
+
+                case 'Conjured Mana Cake':
+                    item = decreaseItemQuality(item)
+                    item = decreaseItemQuality(item)
+
+                    item = condition(isExpired, decreaseItemQuality, identity)(item)
+                    item = condition(isExpired, decreaseItemQuality, identity)(item)
+
                     return item
             
                 default:
                     item = decreaseItemQuality(item)
-
-                    if (isExpired(item)) {
-                        item = decreaseItemQuality(item)
-                    }
-
+                    item = condition(isExpired, decreaseItemQuality, identity)(item)
+                    
                     return item
             }
         })
